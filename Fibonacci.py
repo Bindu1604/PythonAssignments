018073180e834c1397fc49f197627f177d6f5068
Bindu1604 <135733192+Bindu1604@users.noreply.github.com>
2023-06-21 21:10:02 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 21 19:49:09 2023
+
+@author: bindu
+"""
+num = int(input("Enter the number:"))
+n1 = 0 
+n2 = 1
+for i in range(2, num):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3, end=" ")
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
